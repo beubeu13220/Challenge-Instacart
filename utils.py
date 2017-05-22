674a016fab1203e82f099444aa9bb900c5977353
Brehelin <alexandre.brehelin@live.fr>
2017-05-22 22:04:44 +0200

    add approach word2vec

--- utils.py
+++ utils.py
@@ -4,3 +4,13 @@ def descr(data, n=5):
 
 def likely_format(number):
     return '{:,}'.format(number).replace(',', ' ')
+
+    class MySentences(object):
+    def __init__(self, name):
+        self.name = name
+        
+    def run(self):
+        sentence = []
+        for i in range(self.name.shape[0]):
+            sentence.append([self.name.ix[i, "order_id"], self.name.ix[i, "product_id"]])
+        return sentence
