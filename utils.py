70ada33ea359f4c66ebcd2d883d2e1c012169a73
EC2 Default User <ec2-user@ip-172-31-21-103.eu-central-1.compute.internal>
2017-09-09 12:51:31 +0000

    last commit

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os 
+import keras.backend as K
 
 def descr(data, n=5):
     print(data.shape)
@@ -19,4 +20,30 @@ def data_directory():
         
     print(os.getcwd())
 
+
+def pre_score(y_true, y_pred):
+
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0:
+        return 0
+    return c1, c2, c3
+
+def precision(y_true, y_pred):
+    c1, c2, c3 = pre_score(y_true, y_pred)
+    return c1/ c2
+
+def recall(y_true, y_pred):
+    c1, c2, c3 = pre_score(y_true, y_pred)
+    return c1/ c3
+
+def f1_score(y_true, y_pred):
+    precision_s = precision(y_true, y_pred)
+    recall_s = recall(y_true, y_pred)
+    f1  = 2 * (precision_s * recall_s) / (precision_s + recall_s)
+    return f1
     
